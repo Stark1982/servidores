fedba37dd6f11b0ce6e2a25b78adc2e67a7ff377
Stark1982 <patitodehule2025@gmail.com>
2025-07-15 11:20:58 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const http = require('http');
-const horaActual = new Date().toLocaleDateString();
+const fs = require('fs');
+const path = require ('path');
 
-const server = http.createServer((req, res) => {
+/*const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-type', 'text/plain');
     res.end('!Hola¡ este es mi primer servidor con Node.js');
     res.end (`La hora actual es: ${horaActual} `);
     
 });
+*/
+const server = http.createServer((req, res) => {
+    const filePath = Path.join(__dirnamedirname, 'index.html');
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.write.Head(500);
+            res.end('error interno');
+        } else {
+            res.writeHead(200, { 'Content-Type':  'text/html' });
+            res.end(content);
+        }
+
+        });
+         });
+        
+    
+
 
 const port = 3000;
 

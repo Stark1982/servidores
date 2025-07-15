0a3f3a32d4a576ecfa414f3db29254ff75ec6727
Stark1982 <patitodehule2025@gmail.com>
2025-07-15 11:26:29 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,35 +2,21 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-/*const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    res.end('!Hola¡ este es mi primer servidor con Node.js');
-    res.end (`La hora actual es: ${horaActual} `);
-    
-});
-*/
 const server = http.createServer((req, res) => {
-    const filePath = Path.join(__dirnamedirname, 'index.html');
+  const filePath = path.join(__dirname, 'index.html');
 
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            res.write.Head(500);
-            res.end('error interno');
-        } else {
-            res.writeHead(200, { 'Content-Type':  'text/html' });
-            res.end(content);
-        }
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error interno del servidor');
+    }
 
-        });
-         });
-        
-    
-
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(content);
+  });
+});
 
 const port = 3000;
-
 server.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
-    console.log(`texto interno`);
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });

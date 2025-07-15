ad4fb92db7df7e80a9a15b2513325605193f1556
Stark1982 <patitodehule2025@gmail.com>
2025-07-15 11:25:07 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const fs = require('fs');
-const path = require ('path');
+const path = require('path');
 
 /*const server = http.createServer((req, res) => {
     res.statusCode = 200;
